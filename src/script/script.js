c9e054b8b71a11f084a66034da1fbef933159c19
Filipe Margraf <filipemargraf1@gmail.com>
2021-08-26 18:52:13 -0300

    Verifica se as senhas condizem uma com a outra

---
+++ src/script/script.js
@@ -0,0 +1,15 @@
+function passCheck() {
+    var passInp = document.getElementById('passwordInput').value;
+    var confirmPassInp = document.getElementById('confirmPasswordInput').value;
+
+    if (passInp === confirmPassInp) {
+        document.getElementById('passwordInput').style.border = "2px solid #1BE300";
+        document.getElementById('confirmPasswordInput').style.border = "2px solid #1BE300";
+    }
+    else {
+        document.getElementById('passwordInput').style.border = "2px solid #E30000";
+        document.getElementById('confirmPasswordInput').style.border = "2px solid #E30000";
+    }
+}
+
+document.getElementById('buttonCheck').addEventListener('click', passCheck)
