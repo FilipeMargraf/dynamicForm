93a25de801b4577adb34b07cdde9790920a40f38
Filipe Margraf <filipemargraf1@gmail.com>
2021-09-01 18:26:28 -0300

    Adiciona a funçao de validar email e cria o eventListener para o botao

--- src/script/script.js
+++ src/script/script.js
@@ -17,3 +17,20 @@ function passCheck() {
 }
 
 document.getElementById('buttonCheck').addEventListener('click', passCheck)
+
+function emailCheck() {
+    var email = document.getElementById('emailInput').value;
+    var validRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+
+    if (email.match(validRegex)) {
+      document.getElementById('emailInput').style.border = "2px solid #1BE300";
+      document.getElementById('messageMail').innerHTML = ``;
+    }
+    else { 
+      document.getElementById('emailInput').style.border = "2px solid #E30000";
+      document.getElementById('messageMail').innerHTML = `<strong>Email não válido</strong>`
+      document.getElementById('messageMail').style.color = "#E30000";
+    }
+}
+
+document.getElementById('buttonCheck').addEventListener('click', emailCheck);
